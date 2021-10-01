dfd2d38bc7d22ca91efc439d42056f8a3792aee2
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-10-01 14:48:04 +0200

    8. Global styled and add popular, newGames

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // cmponenets
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 
 function App() {
   
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
